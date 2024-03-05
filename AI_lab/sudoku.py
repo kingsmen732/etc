6ea7eacf5c050508e492372aad18d0ba4f7fb85c
B Mithilesh <115350843+kingsmen732@users.noreply.github.com>
2024-03-05 09:22:27 +0000

    meeto

--- AI_lab/sudoku.py
+++ AI_lab/sudoku.py
@@ -1,82 +1,73 @@
-def solve_sudoku(board):
-    # Check if the board is already solved
-    if is_solved(board):
-        return board
-
-    # Find an empty cell
-    row, col = find_empty_cell(board)
-
-    # Try different numbers in the empty cell
-    for num in range(1, 10):
-        if is_valid(board, row, col, num):
-            # Place the number in the cell
-            board[row][col] = num
-
-            # Recursively solve the updated board
-            if solve_sudoku(board):
-                return board
-
-            # If the current placement is not valid, backtrack
-            board[row][col] = 0
-
-    # If no solution is found, return None
-    return None
-
-def is_solved(board):
-    # Check if the board is completely filled
-    # and all the numbers are valid
+def print_board(board):
     for row in board:
-        if 0 in row:
-            return False
-    return True
+        print(" ".join(map(str, row)))
 
-def find_empty_cell(board):
-    # Find the first empty cell in the board
+def find_empty_location(board):
     for row in range(len(board)):
-        for col in range(len(board[row])):
+        for col in range(len(board[0])):
             if board[row][col] == 0:
                 return row, col
     return None, None
 
-def is_valid(board, row, col, num):
-    # Check if placing 'num' in the given cell is valid
-    # Check row
-    for i in range(len(board[row])):
-        if board[row][i] == num:
-            return False
+def used_in_row(board, row, num):
+    return num in board[row]
 
-    # Check column
-    for i in range(len(board)):
-        if board[i][col] == num:
-            return False
+def used_in_col(board, col, num):
+    return num in [board[row][col] for row in range(len(board))]
 
-    # Check 3x3 grid
-    start_row = (row // 3) * 3
-    start_col = (col // 3) * 3
-    for i in range(start_row, start_row + 3):
-        for j in range(start_col, start_col + 3):
-            if board[i][j] == num:
-                return False
+def used_in_box(board, start_row, start_col, num):
+    box_size = int(len(board) ** 0.5)
+    for row in range(start_row, start_row + box_size):
+        for col in range(start_col, start_col + box_size):
+            if board[row][col] == num:
+                return True
+    return False
 
-    return True
+def is_safe(board, row, col, num):
+    box_size = int(len(board) ** 0.5)
+    start_row = row - row % box_size
+    start_col = col - col % box_size
+    return not used_in_row(board, row, num) and not used_in_col(board, col, num) and not used_in_box(board, start_row, start_col, num)
 
-# Example usage
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+def solve_sudoku(board):
+    row, col = find_empty_location(board)
+    if row is None or col is None:
+        return True
+    
+    for num in range(1, len(board) + 1):
+        if is_safe(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
+    
+    return False
+
+board_4x4 = [
+    [0, 2, 0, 0],
+    [0, 0, 6, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 5]
 ]
 
-solution = solve_sudoku(board)
-if solution:
-    print("Sudoku solved:")
-    for row in solution:
-        print(row)
-else:
-    print("No solution found.")
+board_9x9 = [
+    [0, 0, 0, 0, 0, 6, 0, 0, 0],
+    [2, 0, 5, 4, 0, 9, 0, 0, 0],
+    [4, 0, 0, 8, 0, 0, 3, 0, 0],
+    [0, 6, 0, 0, 0, 8, 0, 0, 0],
+    [0, 3, 0, 0, 0, 0, 1, 0, 5],
+    [0, 0, 0, 0, 0, 0, 0, 4, 7],
+    [0, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 9, 0, 6],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+# Solve 4x4 Sudoku
+print("Solving 4x4 Sudoku:")
+solve_sudoku(board_4x4)
+print_board(board_4x4)
+
+# Solve 9x9 Sudoku
+print("\nSolving 9x9 Sudoku:")
+solve_sudoku(board_9x9)
+print_board(board_9x9)
